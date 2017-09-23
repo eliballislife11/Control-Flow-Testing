d77a905affe96428748709aa8bb25c9c65e7308d
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-09-23 02:27:22 -0700

    ProgressBar code. Checks distance and angle of point

--- src/ProgressBar.java
+++ src/ProgressBar.java
@@ -1,4 +1,29 @@
 
 public class ProgressBar {
 
+	public void solve (int percentage, int x, int y) {
+		int degrees = (percentage/100)*360;
+		int radius = 50;
+		double slope = ((y-radius)/(x-radius));
+		double sector = Math.atan((0-slope)/(1+(0*slope)));
+		double distance = Math.sqrt(Math.pow((x-50),2)+Math.pow((y-50),2));
+		System.out.println(distance);
+		System.out.println(slope);
+		System.out.println(sector);
+		if (distance < 50 && sector < degrees) {
+			//compare distance from point to center, if less than 50 than it's within the proximity
+			//of circle, if degree of point to center line is lesser than degree, than its within angle.
+			System.out.println("Red");
+		}
+		else {
+			System.out.println("Blue");
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		ProgressBar p = new ProgressBar();
+		p.solve(25, 0, 0);
+	}
 }
